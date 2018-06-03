1987a9d0d37c7995d382e7140837d8047b4b73b8
Juan <juanbedon26@hotmail.com>
2018-06-03 12:11:12 -0500

    Funcionalidad ejercicio

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import React, { Component } from 'react';
 /// Mostrar una lista en base a un arreglo
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super();
+
     this.state = {
       list: ['Javascript', 'CSS', 'HTLM', 'React']
     }
   }
+
   render() {
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map((elemento_de_la_lista) =>
+            <li>{elemento_de_la_lista}</li>
+            )}
         </ul>
       </div>
     );
